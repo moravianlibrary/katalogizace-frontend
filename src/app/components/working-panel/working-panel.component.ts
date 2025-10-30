50821a896203c0054856c431c9869bb23cf7a26d
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-10-30 09:56:57 +0100

    feat: create editing panel component

---
+++ src/app/components/working-panel/working-panel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-working-panel',
+  imports: [],
+  templateUrl: './working-panel.component.html',
+})
+export class WorkingPanelComponent {}
