e0ef0b9a2c31488cea3f30b61e7cde111cda0d9a
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-10-30 09:54:57 +0100

    feat: create images-view component

---
+++ src/app/components/images-view/images-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-images-view',
+  imports: [],
+  templateUrl: './images-view.component.html',
+})
+export class ImagesViewComponent {}
