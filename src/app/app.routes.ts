65fc3979cb74f1f59c076938ea1a05724468e984
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-10-30 14:09:50 +0100

    feat: update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BooksListComponent } from './components/books-list/books-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: BooksListComponent },
+  // rezervované do budúcna: detail /books/:id atď.
+];
