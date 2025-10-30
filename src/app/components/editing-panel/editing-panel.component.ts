666660bfe0e570d20a1b49d524392977cad0cdb6
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-10-30 09:56:15 +0100

    feat: create editing panel component

---
+++ src/app/components/editing-panel/editing-panel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editing-panel',
+  imports: [],
+  templateUrl: './editing-panel.component.html',
+})
+export class EditingPanelComponent {}
