d8207180527761f9eb5a04c65efd2dbaaaac66db
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-10-31 09:28:02 +0100

    feat: add new books service methods

--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,7 +1,12 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { PaginatedBooksResponse, TaskState } from '../models/book';
+import {
+  BookResultResponse,
+  BookStatusResponse,
+  PaginatedBooksResponse,
+  TaskState,
+} from '../models/book';
 
 @Injectable({ providedIn: 'root' })
 export class BooksService {
@@ -27,5 +32,28 @@ export class BooksService {
     return this.http.get<PaginatedBooksResponse>(`${this.baseUrl}/books/`, {
       params,
     });
+  }
+
+  getBookStatus(bookId: string) {
+    return this.http.get<BookStatusResponse>(
+      `${this.baseUrl}/books/${bookId}/status`,
+    );
+  }
+
+  getBookResult(bookId: string) {
+    return this.http.get<BookResultResponse>(
+      `${this.baseUrl}/books/${bookId}/result`,
+    );
+  }
+
+  getBookImage(
+    bookId: string,
+    imageId: string,
+    opts?: { thumbnail?: boolean },
+  ) {
+    const url = `${this.baseUrl}/books/${bookId}/images/${imageId}`;
+    let params = new HttpParams();
+    if (opts?.thumbnail) params = params.set('thumbnail', 'false');
+    return this.http.get(url, { params, responseType: 'blob' });
   }
 }
