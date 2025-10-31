3a55e2d0013eaed6d37c4f17f5d2bdc01eb0b7cd
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-10-31 09:27:18 +0100

    feat: create new route for book detail view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { BookDetailComponent } from './components/book-detail/book-detail.component';
 import { BooksListComponent } from './components/books-list/books-list.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'books', pathMatch: 'full' },
   { path: 'books', component: BooksListComponent },
+  { path: 'books/:bookId', component: BookDetailComponent },
   // rezervované do budúcna: detail /books/:id atď.
 ];
