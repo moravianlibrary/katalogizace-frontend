ab99fb140682118413fd0745908dd3153c3b6550
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-11-03 14:37:48 +0100

    feat: update working panel component and book detail view

--- src/app/components/working-panel/working-panel.component.ts
+++ src/app/components/working-panel/working-panel.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ExistingMarcRecord } from '../../models/book';
+import { MarcRecordsTabsComponent } from '../marc-records/marc-records.component';
 
 @Component({
   standalone: true,
   selector: 'app-working-panel',
-  imports: [],
+  imports: [MarcRecordsTabsComponent],
   templateUrl: './working-panel.component.html',
 })
-export class WorkingPanelComponent {}
+export class WorkingPanelComponent {
+  existingRecords = input<ExistingMarcRecord[]>([]);
+}
