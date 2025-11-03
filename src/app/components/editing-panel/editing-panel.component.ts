14af7c6d82997c236cbb3191630d0d9e0565610b
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-11-03 15:39:39 +0100

    feat: add navigation buttons to editing panel and update scrolling behavior

--- src/app/components/editing-panel/editing-panel.component.ts
+++ src/app/components/editing-panel/editing-panel.component.ts
@@ -1,11 +1,12 @@
 import { JsonPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { BookResultResponse } from '../../models/book';
+import { NavigationButtonsComponent } from '../navigation-buttons/navigation-buttons.component';
 
 @Component({
   standalone: true,
   selector: 'app-editing-panel',
-  imports: [JsonPipe],
+  imports: [JsonPipe, NavigationButtonsComponent],
   templateUrl: './editing-panel.component.html',
 })
 export class EditingPanelComponent {
