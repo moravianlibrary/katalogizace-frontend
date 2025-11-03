2f879ef31913c4e619470da423b176741bbab5c8
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-11-03 14:35:40 +0100

    chore: remove unused host object

--- src/app/components/marc-row-normal/marc-row-normal.component.ts
+++ src/app/components/marc-row-normal/marc-row-normal.component.ts
@@ -3,7 +3,6 @@ import { Component, input } from '@angular/core';
 @Component({
   standalone: true,
   selector: 'tr[appMarcRowNormal]',
-  host: { class: 'border-b-[0.5px] border-gray-500 align-top' },
   templateUrl: './marc-row-normal.component.html',
 })
 export class MarcRowNormalComponent {
