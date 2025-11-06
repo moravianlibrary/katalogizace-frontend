fc66825e414de3890e6eabb2ff4fc96957c7fbe1
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-11-06 11:34:40 +0100

    feat: add image thumbnails component

---
+++ src/app/components/image/thumbnails/thumbnails.component.ts
@@ -0,0 +1,21 @@
+// src/app/components/book-detail/thumbnails.component.ts
+import { CommonModule } from '@angular/common';
+import { Component, input, output } from '@angular/core';
+import { ImgItem } from './../../../models/book';
+
+@Component({
+  standalone: true,
+  selector: 'app-image-thumbnails',
+  templateUrl: './thumbnails.component.html',
+  imports: [CommonModule],
+})
+export class ImageThumbnailsComponent {
+  items = input.required<ImgItem[]>();
+  selectedId = input<string | null>(null);
+
+  select = output<string>();
+
+  onPick(id: string) {
+    this.select.emit(id);
+  }
+}
