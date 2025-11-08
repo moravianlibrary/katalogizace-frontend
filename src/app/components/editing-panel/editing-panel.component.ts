990298eb7aa74771906675294fd500ee983f9160
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-11-08 15:02:12 +0100

    feat: add exctracted fields to editing panel

--- src/app/components/editing-panel/editing-panel.component.ts
+++ src/app/components/editing-panel/editing-panel.component.ts
@@ -1,12 +1,13 @@
 import { JsonPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { BookResultResponse } from '../../models/book';
+import { ExtractedFieldsComponent } from '../extracted-fields/extracted-fields/extracted-fields.component';
 import { NavigationButtonsComponent } from '../navigation-buttons/navigation-buttons.component';
 
 @Component({
   standalone: true,
   selector: 'app-editing-panel',
-  imports: [JsonPipe, NavigationButtonsComponent],
+  imports: [JsonPipe, NavigationButtonsComponent, ExtractedFieldsComponent],
   templateUrl: './editing-panel.component.html',
 })
 export class EditingPanelComponent {
