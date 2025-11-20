421f718c092495f2c6f053852ce14b51c15ded33
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-11-20 12:03:52 +0100

    feat: add LEADER field to MARC record table

--- src/app/components/marc-record-table/marc-record-table.component.ts
+++ src/app/components/marc-record-table/marc-record-table.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { ExistingMarcRecord } from '../../models/book';
+import { MarcRowLeaderComponent } from '../marc-row-leader/marc-row-leader.component';
 import { MarcRowNormalComponent } from '../marc-row-normal/marc-row-normal.component';
 import { MarcRowSpecialComponent } from '../marc-row-special/marc-row-special.component';
 
 @Component({
   standalone: true,
   selector: 'app-marc-record-table',
-  imports: [MarcRowSpecialComponent, MarcRowNormalComponent, CommonModule],
+  imports: [
+    MarcRowSpecialComponent,
+    MarcRowNormalComponent,
+    MarcRowLeaderComponent,
+    CommonModule,
+  ],
   templateUrl: './marc-record-table.component.html',
 })
 export class MarcRecordTableComponent {
