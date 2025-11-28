171b8be98c9271ac3b891e9ba80eb66b452399bd
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2025-11-28 10:54:09 +0100

    refactor: move marc-row-* components to marc-row folder

--- src/app/components/marc-record-table/marc-record-table.component.ts
+++ src/app/components/marc-record-table/marc-record-table.component.ts
@@ -1,9 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { ExistingMarcRecord } from '../../models/book';
-import { MarcRowLeaderComponent } from '../marc-row-leader/marc-row-leader.component';
-import { MarcRowNormalComponent } from '../marc-row-normal/marc-row-normal.component';
-import { MarcRowSpecialComponent } from '../marc-row-special/marc-row-special.component';
+import { MarcRowLeaderComponent } from '../marc-row/marc-row-leader/marc-row-leader.component';
+import { MarcRowNormalComponent } from '../marc-row/marc-row-normal/marc-row-normal.component';
+import { MarcRowSpecialComponent } from '../marc-row/marc-row-special/marc-row-special.component';
 
 @Component({
   standalone: true,
