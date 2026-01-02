61bb1a42871f51960f3f99daa71b70bc32c38a07
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2026-01-02 16:11:33 +0100

    feat: add batches service

---
+++ src/app/services/batches.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Batch, BatchesResponse } from '../models/book';
+import { EnvironmentService } from './environment.service';
+
+@Injectable({ providedIn: 'root' })
+export class BatchesService {
+  private http = inject(HttpClient);
+  private env = inject(EnvironmentService);
+
+  private get apiBaseUrl(): string {
+    return this.env.get('apiServiceBaseUrl') as string;
+  }
+
+  listBatches(opts: { filter_owned_by_user?: boolean } = {}) {
+    const { filter_owned_by_user = false } = opts;
+
+    let params = new HttpParams();
+    if (filter_owned_by_user) {
+      params = params.set('filter_owned_by_user', 'true');
+    }
+
+    return this.http.get<BatchesResponse>(`${this.apiBaseUrl}/batches/`, {
+      params,
+    });
+  }
+
+  getBatch(batch_id: string) {
+    return this.http.get<Batch>(`${this.apiBaseUrl}/batches/${batch_id}`);
+  }
+
+  createBatch(name: string, description: string | null) {
+    let params = new HttpParams().set('name', name);
+
+    if (description !== null) {
+      params = params.set('description', description);
+    }
+
+    return this.http.post<Batch>(`${this.apiBaseUrl}/batches/`, null, {
+      params,
+    });
+  }
+
+  deleteBatch(batch_id: string) {
+    return this.http.delete(`${this.apiBaseUrl}/batches/${batch_id}`);
+  }
+}
