45a11c62b11a35a0c3535daff7e1b9add57a02f2
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2026-02-16 09:55:50 +0100

    feat: create context panel header component

---
+++ src/app/components/context-panel/context-panel-header/context-panel-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-context-panel-header',
+  imports: [],
+  templateUrl: './context-panel-header.component.html',
+})
+export class ContextPanelHeaderComponent {}
