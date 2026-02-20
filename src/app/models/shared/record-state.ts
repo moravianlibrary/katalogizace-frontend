6d9101886be513a03bae010718aee1345a8f353a
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2026-02-20 15:16:21 +0100

    feat: add indicators to QuickAddItem

--- src/app/models/shared/record-state.ts
+++ src/app/models/shared/record-state.ts
@@ -3,6 +3,8 @@ import { FieldType } from '../books/record-view.model';
 
 export interface QuickAddItem {
   tag: number;
+  ind1: string;
+  ind2: string;
   subfields: MarcSubfield[];
   repeatable: boolean;
   type: FieldType;
