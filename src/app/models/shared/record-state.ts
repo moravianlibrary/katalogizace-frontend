461eb819e4c9e4da176945ac3659ec217a4be784
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2026-02-20 14:36:36 +0100

    feat: extend QuickAddItem

--- src/app/models/shared/record-state.ts
+++ src/app/models/shared/record-state.ts
@@ -1,7 +1,9 @@
+import { MarcSubfield } from '../books/marc.dto';
 import { FieldType } from '../books/record-view.model';
 
 export interface QuickAddItem {
   tag: number;
+  subfields: MarcSubfield[];
   repeatable: boolean;
   type: FieldType;
 }
