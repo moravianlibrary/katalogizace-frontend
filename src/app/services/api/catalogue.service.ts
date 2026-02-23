bcaa3fdb52231572fbdd513c212955a6b5ceba1c
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2026-02-23 10:44:17 +0100

    feat: add anakon autocomplete function

--- src/app/services/api/catalogue.service.ts
+++ src/app/services/api/catalogue.service.ts
@@ -1,5 +1,9 @@
-import { ExistingMarcRecord } from '@/app/models/';
-import { HttpClient } from '@angular/common/http';
+import {
+  AutocompleteResponse,
+  CatalogueBase,
+  ExistingMarcRecord,
+} from '@/app/models/';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { EnvironmentService } from '../environment.service';
@@ -21,5 +25,28 @@ export class CatalogueService {
         `${this.apiBaseUrl}/catalogue/providers/${provider}/authorities/${recordId}`,
       )
       .pipe(map((resp) => resp.record));
+  }
+
+  autocompleteAnakon(opts: {
+    query: string;
+    field: string;
+    subfield: string;
+    limit?: number;
+    bases: CatalogueBase[];
+  }) {
+    const { query, field, subfield, limit = 20, bases } = opts;
+
+    let params = new HttpParams()
+      .set('query', query)
+      .set('field', field)
+      .set('subfield', subfield)
+      .set('limit', String(limit));
+
+    for (const b of bases) params = params.append('bases', b);
+
+    return this.http.get<AutocompleteResponse>(
+      `${this.apiBaseUrl}/catalogue/providers/anakon/autocomplete`,
+      { params },
+    );
   }
 }
