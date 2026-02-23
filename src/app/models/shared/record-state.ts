caefea04285ea6d55bba1dd864a7d06592a56557
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2026-02-23 13:02:17 +0100

    feat: outsource quick add items

--- src/app/models/shared/record-state.ts
+++ src/app/models/shared/record-state.ts
@@ -9,3 +9,91 @@ export interface QuickAddItem {
   repeatable: boolean;
   type: FieldType;
 }
+
+export const QUICK_ADD: QuickAddItem[] = [
+  {
+    tag: 100,
+    ind1: '',
+    ind2: '',
+    subfields: [
+      { code: 'a', value: '' },
+      { code: 'd', value: '' },
+      { code: '7', value: '' },
+      { code: '4', value: '' },
+    ],
+    repeatable: false,
+    type: 'data' as FieldType,
+  },
+  {
+    tag: 245,
+    ind1: '',
+    ind2: '',
+    subfields: [
+      { code: 'a', value: '' },
+      { code: 'b', value: '' },
+      { code: 'c', value: '' },
+    ],
+    repeatable: false,
+    type: 'data' as FieldType,
+  },
+  {
+    tag: 255,
+    ind1: '',
+    ind2: '',
+    subfields: [{ code: 'a', value: '' }],
+    repeatable: true,
+    type: 'data' as FieldType,
+  },
+  {
+    tag: 264,
+    ind1: '',
+    ind2: '',
+    subfields: [
+      { code: 'b', value: '' },
+      { code: 'c', value: '' },
+    ],
+    repeatable: true,
+    type: 'data' as FieldType,
+  },
+  {
+    tag: 300,
+    ind1: '',
+    ind2: '',
+    subfields: [
+      { code: 'a', value: '' },
+      { code: 'b', value: '' },
+    ],
+    repeatable: true,
+    type: 'data' as FieldType,
+  },
+  {
+    tag: 500,
+    ind1: '',
+    ind2: '',
+    subfields: [{ code: 'a', value: '' }],
+    repeatable: true,
+    type: 'data' as FieldType,
+  },
+  {
+    tag: 651,
+    ind1: '',
+    ind2: '7',
+    subfields: [
+      { code: 'a', value: '' },
+      { code: '2', value: 'czenas' },
+    ],
+    repeatable: true,
+    type: 'data' as FieldType,
+  },
+  {
+    tag: 655,
+    ind1: '',
+    ind2: '7',
+    subfields: [
+      { code: 'a', value: '' },
+      { code: '2', value: 'czenas' },
+    ],
+    repeatable: true,
+    type: 'data' as FieldType,
+  },
+];
--- src/app/services/record-state.service.ts
+++ src/app/services/record-state.service.ts
@@ -12,7 +12,7 @@ import {
   UUID,
 } from '@/app/models';
 import { computed, Injectable, signal } from '@angular/core';
-import { QuickAddItem } from '../models/shared/record-state';
+import { QUICK_ADD } from '../models/shared/record-state';
 import {
   existingToEditableWithMeta,
   extractedToEditableWithMeta,
@@ -73,93 +73,7 @@ export class RecordStateService {
     );
   });
 
-  readonly QUICK_ADD: QuickAddItem[] = [
-    {
-      tag: 100,
-      ind1: '',
-      ind2: '',
-      subfields: [
-        { code: 'a', value: '' },
-        { code: 'd', value: '' },
-        { code: '7', value: '' },
-        { code: '4', value: '' },
-      ],
-      repeatable: false,
-      type: 'data' as FieldType,
-    },
-    {
-      tag: 245,
-      ind1: '',
-      ind2: '',
-      subfields: [
-        { code: 'a', value: '' },
-        { code: 'b', value: '' },
-        { code: 'c', value: '' },
-      ],
-      repeatable: false,
-      type: 'data' as FieldType,
-    },
-    {
-      tag: 255,
-      ind1: '',
-      ind2: '',
-      subfields: [{ code: 'a', value: '' }],
-      repeatable: true,
-      type: 'data' as FieldType,
-    },
-    {
-      tag: 264,
-      ind1: '',
-      ind2: '',
-      subfields: [
-        { code: 'b', value: '' },
-        { code: 'c', value: '' },
-      ],
-      repeatable: true,
-      type: 'data' as FieldType,
-    },
-    {
-      tag: 300,
-      ind1: '',
-      ind2: '',
-      subfields: [
-        { code: 'a', value: '' },
-        { code: 'b', value: '' },
-      ],
-      repeatable: true,
-      type: 'data' as FieldType,
-    },
-    {
-      tag: 500,
-      ind1: '',
-      ind2: '',
-      subfields: [{ code: 'a', value: '' }],
-      repeatable: true,
-      type: 'data' as FieldType,
-    },
-    {
-      tag: 651,
-      ind1: '',
-      ind2: '7',
-      subfields: [
-        { code: 'a', value: '' },
-        { code: '2', value: 'czenas' },
-      ],
-      repeatable: true,
-      type: 'data' as FieldType,
-    },
-    {
-      tag: 655,
-      ind1: '',
-      ind2: '7',
-      subfields: [
-        { code: 'a', value: '' },
-        { code: '2', value: 'czenas' },
-      ],
-      repeatable: true,
-      type: 'data' as FieldType,
-    },
-  ];
+  readonly QUICK_ADD = QUICK_ADD;
 
   readonly visibleQuickAdd = computed(() => {
     const existing = new Set<number>();
