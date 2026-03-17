6537815abe7c0239bec4159ce947b067c9dfb463
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2026-03-17 13:34:39 +0100

    feat: add lock and unlock icon component

---
+++ src/app/components/shared/lock-hover-icon/lock-hover-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'app-lock-hover-icon',
+  templateUrl: './lock-hover-icon.component.html',
+})
+export class LockHoverIconComponent {}
