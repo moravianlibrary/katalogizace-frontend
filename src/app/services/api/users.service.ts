5dc38e22b73e864a10f6013e2e65343faa77eae2
Tomáš Karol Hőger <tomaskarol.hoger@gmail.com>
2026-05-02 22:30:48 +0200

    feat: add users service

---
+++ src/app/services/api/users.service.ts
@@ -0,0 +1,46 @@
+import {
+  UserCreateDto,
+  UserDto,
+  UserInfoDto,
+  UserUpdateDto,
+} from '@/app/models';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EnvironmentService } from '../environment.service';
+
+@Injectable({ providedIn: 'root' })
+export class UsersService {
+  private http = inject(HttpClient);
+  private env = inject(EnvironmentService);
+
+  private get apiBaseUrl(): string {
+    return this.env.get('apiServiceBaseUrl') as string;
+  }
+
+  listUsers(view: 'basic'): Observable<UserInfoDto[]>;
+  listUsers(view: 'detail'): Observable<UserDto[]>;
+  listUsers(view: 'basic' | 'detail' = 'basic') {
+    const params = new HttpParams().set('view', view);
+
+    return this.http.get<UserInfoDto[] | UserDto[]>(
+      `${this.apiBaseUrl}/users/`,
+      { params },
+    );
+  }
+
+  createUser(dto: UserCreateDto) {
+    return this.http.post<UserInfoDto>(
+      `${this.apiBaseUrl}/users/register`,
+      dto,
+    );
+  }
+
+  updateUser(userId: number, dto: UserUpdateDto) {
+    return this.http.patch(`${this.apiBaseUrl}/users/${userId}`, dto);
+  }
+
+  deleteUser(userId: number) {
+    return this.http.delete(`${this.apiBaseUrl}/users/${userId}`);
+  }
+}
